20bd2393bc5e667995f0b3fd7eb7c402f56054a2
TheFox1998 <MZrG3gzjLBUA>
2021-02-06 20:54:06 +0300

    feat(Schedule:models): Сделать модель расписания
    Ура! Ура! Ура! Товарищи, мы это сделали! Хотел бы поздравить весь наш
    коллектив с созданием модели расписания.

--- Schedule/admin.py
+++ Schedule/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from Schedule.models import Note, Schedule
+from Schedule.models import Schedule
 
 
 @admin.register(Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'note_id', 'user_id']
-
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ['id', 'content', 'date_add', 'dead_line', 'day_on_weak']
+    fields = ('user_id', 'time', 'date', 'affair', 'note', 'homework', 'is_ready')
--- Schedule/models.py
+++ Schedule/models.py
@@ -4,22 +4,21 @@ from django.db import models
 
 
 # Create your models here.
+
 class Schedule(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='пользователь')
-    note = models.ForeignKey('Note', on_delete=models.PROTECT, verbose_name='заметка')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='id пользователя', unique=False, help_text='Id пользователя должен быть в виде одного числа')
+    time = models.CharField(verbose_name='время', max_length=100, unique=False, help_text='Время в формате h:m-h:m')
+    date = models.DateField(verbose_name='дата', unique=False, help_text='Дата в формате YYYY-MM-DD')
+    affair = models.CharField(verbose_name='дело', max_length=200, unique=False, help_text='Дела должны быть в виде обычного текста')
+    note = models.CharField(verbose_name='заметка', max_length=200, unique=False, help_text='Заметки должны быть в виде обычного текста')
+    homework = models.CharField(verbose_name='домашнее задание', max_length=200, unique=False, default=None, help_text='Домашнее задание должны быть в виде обычного текста')
+    is_ready = models.BooleanField(verbose_name='готово?', default=False, help_text='Готово? принимает только значение True или False')
 
+    def __str__(self):
+        return f"{self.user_id.username} - это его расписание"
 
-class Note(models.Model):
-    class DayWeak(models.TextChoices):
-        MON = ('Пн', 'Понедельник')
-        TUE = ('Вт', 'Вторник')
-        WEN = ('Ср', 'Среда')
-        THUR = ('Чт', 'Четверг')
-        FRI = ('Пт', 'Пятница')
-        SAT = ('Сб', 'Суббота')
-        SUN = ('Вс', 'Воскресенье')
-
-    content = models.TextField(verbose_name='контент')
-    date_add = models.DateTimeField(verbose_name='дата добавления', auto_now_add=True)
-    dead_line = models.DateTimeField(verbose_name='дед лайн')
-    day_on_weak = models.CharField(choices=DayWeak.choices, verbose_name='день недели', max_length=2)
+    class Meta:
+        managed = True
+        verbose_name = 'расписание'
+        verbose_name_plural = 'расписании'
+        ordering = ['date', ]
