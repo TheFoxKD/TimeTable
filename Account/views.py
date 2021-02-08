e0ab9c8b67f050aa9ff7b927c353571237d36ab7
TheFox1998 <MZrG3gzjLBUA>
2021-02-08 18:03:02 +0300

    feat(Account:sign_in,sign_up): Сделать регистрацию и вход в одном стиле

--- Account/views.py
+++ Account/views.py
@@ -2,9 +2,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render
 from django.urls import reverse_lazy
-
 from django.views.generic import CreateView
 
 from Account.forms import SignInAccountForm, SignUpAccountForm
@@ -14,20 +12,26 @@ from Account.forms import SignInAccountForm, SignUpAccountForm
 class SignUpAccount(SuccessMessageMixin, CreateView):
     """
     Работаем с моделью User
-    Используем форму RegisterAccountForm
-    Html шаблон показыем registration.html
+    Используем форму SignUpAccountForm
+    Html шаблон показыем sign_up.html
     Если регистрация успешно сделана, то выводим передаём сообщение "Вы успешно зарегистрировались"
     Если регистрация успешно сделана, то переходим по ссылке на страницу авторизации
     """
     model = User
     form_class = SignUpAccountForm
     template_name = 'Account/sign_up.html'
-    success_message = f'Вы успешно зарегистрировались'
+    success_message = f'Вы успешно прошли регистрацию'
     success_url = reverse_lazy('Account:sign_in')
 
 
 class SignInAccount(SuccessMessageMixin, LoginView):
+    """
+    Используем форму SignInAccountForm
+    Html шаблон показыем sign_in.html
+    Если регистрация успешно сделана, то выводим передаём сообщение "Вы успешно вошли в свой аккаунт"
+    Если авторизация успешно пройдена, то переходим по ссылке на страницу авторизации
+    """
     form_class = SignInAccountForm
     template_name = 'Account/sign_in.html'
-    success_message = f'Вы успешно авторизировались'
+    success_message = f'Вы успешно вошли в свой аккаунт'
     success_url = reverse_lazy('Account:sign_in')
