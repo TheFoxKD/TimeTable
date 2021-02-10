5a1e93d6997de0459d8f9921fa0475d0902bba44
TheFox1998 <MZrG3gzjLBUA>
2021-02-10 20:05:17 +0300

    feat(Account:sign_in_giseo): Сделать валидацию формы входа в giseo
    Сделана проверка формы подключения giseo. Поля: Городской округ /
    Муниципальный район, Населённый пункт.

--- Account/apps.py
+++ Account/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 
 class AccountConfig(AppConfig):
     name = 'Account'
+    verpose_name = 'аккаунт'
+    verpose_name_plural = 'аккаунты'
--- Account/models.py
+++ Account/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 # Create your models here.
 class Giseo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
-    login = models.CharField(max_length=150, verbose_name='логин')
-    password = models.CharField(max_length=100, verbose_name='пароль')
-    place = models.CharField(max_length=150, verbose_name='городской округ / Муниципальный район')
-    locality = models.CharField(max_length=150, verbose_name='населённый пункт')
-    type_of_oo = models.CharField(max_length=150, verbose_name='тип ОО')
-    educational_organization = models.CharField(max_length=150, verbose_name='образовательная организация')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь', blank=False)
+    login = models.CharField(max_length=150, verbose_name='логин', blank=False)
+    password = models.CharField(max_length=100, verbose_name='пароль', blank=False)
+    place = models.CharField(max_length=150, verbose_name='городской округ / Муниципальный район', blank=False)
+    locality = models.CharField(max_length=150, verbose_name='населённый пункт', blank=False)
+    type_of_oo = models.CharField(max_length=150, verbose_name='тип ОО', blank=False)
+    educational_organization = models.CharField(max_length=150, verbose_name='образовательная организация', blank=False)
 
     def __str__(self):
         return f"{self.login} - {self.user.username}"
