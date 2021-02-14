f2051d312bbb9330390af4c9175c3b95211257e7
TheFox1998 <MZrG3gzjLBUA>
2021-02-14 13:25:07 +0300

    style(Account,Schedule): Добавить docstring

--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,5 @@
+#  Copyright (c) 2021. TheFox
+
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
@@ -14,11 +16,10 @@ from Account.models import Giseo
 
 class SignUpAccount(SuccessMessageMixin, CreateView):
     """
-    Работаем с моделью User
-    Используем форму SignUpAccountForm
-    Html шаблон показыем sign_up.html
-    Если регистрация успешно сделана, то выводим передаём сообщение "Вы успешно зарегистрировались"
-    Если регистрация успешно сделана, то переходим по ссылке на страницу авторизации
+    model = User - это модель, с которой работает данная страница
+    form_class = SignUpAccountForm -это форма, с которой работает данная страница
+    template_name = Account/sign_up.html - это HTML код, который вызывается при загрузке данной страницы
+    success_message = Вы успешно прошли регистрацию - это текст сообщения, которое передаётся в message с указателем success
     """
     model = User
     form_class = SignUpAccountForm
@@ -26,34 +27,62 @@ class SignUpAccount(SuccessMessageMixin, CreateView):
     success_message = f'Вы успешно прошли регистрацию'
 
     def get_success_url(self):
+        """
+        Данная функция после успешного заполнения формы перенаправляет на страницу авторизации
+        :return: Redirect на страницу авторизации
+        :rtype:
+        """
         return reverse_lazy('Account:sign_in')
 
 
 class SignInAccount(SuccessMessageMixin, LoginView):
     """
-    Используем форму SignInAccountForm
-    Html шаблон показыем sign_in.html
-    Если регистрация успешно сделана, то выводим передаём сообщение "Вы успешно вошли в свой аккаунт"
-    Если авторизация успешно пройдена, то переходим по ссылке на страницу авторизации
+    form_class = SignInAccountForm - это форма, с которой работает данная страница
+    template_name = Account/sign_in.html - это HTML код, который вызывается при загрузке данной страницы
+    success_message = Вы успешно вошли в свой аккаунт - это текст сообщения, которое передаётся в message с указателем success
     """
     form_class = SignInAccountForm
     template_name = 'Account/sign_in.html'
     success_message = f'Вы успешно вошли в свой аккаунт'
 
     def get_success_url(self):
+        """
+        Данная функция после успешного заполнения формы перенаправляет на страницу расаписания пользователя, который совершает запрос
+        :return: Redirect на страницу расписания
+        :rtype:
+        """
         return reverse_lazy('Schedule:schedule', kwargs={'user_id': self.request.user.id})
 
 
 class SignInGiseo(LoginRequiredMixin, SuccessMessageMixin, FormView):
+    """
+    model = Giseo - это модель, с которой работает данная страница
+    form_class = SignInGiseoForm - это форма, с которой работает данная странциа
+    template_name = Account/sign_in_giseo.html - это HTML код, который вызывается при загрузке данной страницы
+    success_message = Вы успешно подключили giseo к своему аккаунту - это текст сообщения, которое передаётся в message с указателем success
+    """
     model = Giseo
     form_class = SignInGiseoForm
     template_name = 'Account/sign_in_giseo.html'
     success_message = f'Вы успешно подключили giseo к своему аккаунту'
 
     def get_success_url(self):
+        """
+        Данная функция после успешного заполнения формы перенаправляет на страницу расаписания пользователя, который совершает запрос
+        :return: Redirect на страницу расписания
+        :rtype:
+        """
         return reverse_lazy('Schedule:schedule', kwargs={'user_id': self.request.user.id})
 
     def form_valid(self, form):
+        """
+        Данная функция выполняется, если форма прошла валидацию. Данная функция совершает попытку получить объект с user = пользователю, который совершает данный запрос. Если
+        объект с данным пользователем найден, то мы обновляем поля в данного объекта, а не создаём новую запись в бд. Если ошибка, то мы создаём новую запись с данными из формы
+        :param form:
+        :type form:
+        :return: form
+        :rtype:
+        """
         try:
             post = Giseo.objects.get(user=self.request.user)
             post.login = form.instance.login
--- Schedule/views.py
+++ Schedule/views.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+#  Copyright (c) 2021. TheFox
+
 # Create your views here.
 import datetime
 
@@ -13,36 +16,82 @@ from Schedule.models import Schedule
 
 
 class DetailSchedule(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    """
+    Страница расписания для определённого пользователя с возможностью создания на ней нового дела
+    model = Schedule - модель, которую использует данная страница
+    template_name = Schedule/schedule.html - шаблон, который использует страница
+    context_object_name = schedule - переменная, которая используеися в HTML коде (не нужна)
+    form_class = CreateAffairScheduleForm - форма, которая используется на этой странице
+    """
     model = Schedule
     template_name = 'Schedule/schedule.html'
     context_object_name = 'schedule'
     form_class = CreateAffairScheduleForm
 
     def form_valid(self, form):
+        """
+        Данная функция вызывается, если форма прошла валидацию. Когда данная фукнция вызывается мы подставляем в форму id пользователя, чтобы далее он записался в бд
+        :param form:
+        :type form:
+        :return: form
+        :rtype:
+        """
         form.instance.user = User.objects.get(pk=self.kwargs['user_id'])
         return super(DetailSchedule, self).form_valid(form)
 
     def test_func(self):
+        """
+        Данная функция вызывается при загрузке страницы. Данная функция проверяет есть ли доступ у пользователя к данной странице. У нас два условия, чтобы пользователь получил
+        доступ: 1. Если id пользователя в url строке соответсвует id пользователя, который совершает данный запрос 2. Если пользователь, который совершает  имеет статус stuff = 1
+        :return: True or False
+        :rtype:
+        """
         if self.kwargs['user_id'] == self.request.user.id:
             return True
         elif self.request.user.is_staff:
             return True
 
     def get_success_url(self):
+        """
+        Данная функция вызывается после успешного POST запроса. Данная функция перенаправляет на пользователя на страницу с его расписанием после создания нового дела
+        :return: Redirect на опр. страницу
+        :rtype:
+        """
         return reverse_lazy('Schedule:schedule', kwargs={'user_id': self.kwargs.pop('user_id', None)})
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        """
+        Данная функция вызывается после получения пользователя доступа к странице. Тут фильтруется модель под рамки начала недели и конца недели. Разбивается на каждый день
+        недели и далее в HTML коде используется для получения дел для каждого дня
+        :param object_list:
+        :type object_list:
+        :param kwargs:
+        :type kwargs:
+        :return: context
+        :rtype:
+        """
         context = super(DetailSchedule, self).get_context_data(**kwargs)
         date = datetime.date.today()
         start_week = date - datetime.timedelta(date.weekday())
         end_week = start_week + datetime.timedelta(7)
-        context['mon'] = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week], date__week_day=2).order_by('time_start')
-        context['tue'] = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week], date__week_day=3).order_by('time_start')
-        context['wed'] = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week], date__week_day=4).order_by('time_start')
-        context['thu'] = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week], date__week_day=5).order_by('time_start')
-        context['fri'] = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week], date__week_day=6).order_by('time_start')
-        context['sat'] = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week], date__week_day=7).order_by('time_start')
-        context['sun'] = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week], date__week_day=1).order_by('time_start')
+        # Для расписания
+        model = Schedule.objects.filter(user=self.kwargs['user_id'], date__range=[start_week, end_week])
+        context['mon'] = model.filter(date__week_day=2).order_by('time_start')
+        context['tue'] = model.filter(date__week_day=3).order_by('time_start')
+        context['wed'] = model.filter(date__week_day=4).order_by('time_start')
+        context['thu'] = model.filter(date__week_day=5).order_by('time_start')
+        context['fri'] = model.filter(date__week_day=6).order_by('time_start')
+        context['sat'] = model.filter(date__week_day=7).order_by('time_start')
+        context['sun'] = model.filter(date__week_day=1).order_by('time_start')
+        # Для дат
+        date_generated = [start_week + datetime.timedelta(days=x) for x in range(0, (end_week - start_week).days)]
+        context['mon_date'] = date_generated[0]
+        context['tue_date'] = date_generated[1]
+        context['wed_date'] = date_generated[2]
+        context['thu_date'] = date_generated[3]
+        context['fri_date'] = date_generated[4]
+        context['sat_date'] = date_generated[5]
+        context['sun_date'] = date_generated[6]
         return context
 
 
