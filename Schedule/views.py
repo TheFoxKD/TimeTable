3c6c481d35dcd3244a5a5e822afaa2b18de7e7a3
artem <artemon17092006@mail.ru>
2021-02-22 15:35:22 +0300

    fix(parser):
    fix bug in parsing

--- Schedule/views.py
+++ Schedule/views.py
@@ -160,7 +160,7 @@ class DeleteAffairSchedule(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 def test(request):
-    giseo_obj = Giseo.objects.get(user=1)
+    giseo_obj = Giseo.objects.get(user=)
     objects = parsing(giseo_obj.place.name, giseo_obj.locality.name, giseo_obj.type_of_oo.name, giseo_obj.educational_organization.name, giseo_obj.login, giseo_obj.password)
     print(objects)
     # Schedule.objects.bulk_create(sch)
