551c7435de96f721a3a3263725e68851100bea13
artem <artemon17092006@mail.ru>
2021-04-26 22:55:45 +0300

    style(index/style): change main page style and images

--- TimeTable/settings.py
+++ TimeTable/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('KEY')
+SECRET_KEY = ')n=013e0c&x49ckd$7e^@tf31xomsywgzy(fe_p-hsu(#@eh9z'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.mysite.com']
 
